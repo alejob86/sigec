62de2da19eeb6871869cb9cd2d4c5f390e8c86b6
alejob86 <alejandrobernalcollazos@gmail.com>
2015-08-08 13:05:25 -0300

    testing modal

--- resources/views/people/list.blade.php
+++ resources/views/people/list.blade.php
@@ -8,6 +8,7 @@
 	            <header class="panel-heading ">
 	                Responsive Data Table
 	                <span class="tools pull-right">
+	                	<a class="pull-right" data-toggle="modal" href="#forgotPass"> Modal</a>
 	                    <a class="fa fa-repeat box-refresh" href="javascript:;"></a>
 	                    <a class="t-close fa fa-times" href="javascript:;"></a>
 	                </span>
@@ -30,12 +31,6 @@
 	                <th>
 	                    Units
 	                </th>
-	                <th>
-	                    Unit Cost
-	                </th>
-	                <th>
-	                    Total
-	                </th>
 	            </tr>
 	            </thead>
 	            <tbody>
@@ -55,12 +50,6 @@
 	                <td>
 	                    95
 	                </td>
-	                <td>
-	                    1.99
-	                </td>
-	                <td>
-	                    189.05
-	                </td>
 	            </tr>
 	            </tbody>
 	            </table>
@@ -68,4 +57,26 @@
 	    </div>
 	</div>
 
+
+	<!-- Modal -->
+    <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="forgotPass" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Forgot Password ?</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Enter your e-mail address below to reset your password.</p>
+                    <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+
+                </div>
+                <div class="modal-footer">
+                    <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+                    <button class="btn btn-success" type="button">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- modal -->
 @stop
